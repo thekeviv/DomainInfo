c37929e196a23034e38136c532fb21fbc9665f2b
Vivek Sharma <hello@viveksharma.dev>
2021-03-17 01:32:25 -0700

    Initial Commit ✨

---
+++ domaininfo.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# The script gathers info about a domain name supplied to it and prints it in an 
+# easily consumable manner.
+# Author - Vivek Sharma
+# Written for the Lab Assignment of SENG460 at the University of Victoria taken in Spring 2021
+
+
+if [ $# -gt 1 -o $# -lt 1 ]
+then 
+    echo "Error: Only one argument allowed. The expected input for the command is domaininfo {domain name}"
+else
+    echo "Lol"
+fi
